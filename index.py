6c006b8c81ea5f390cd1cd7af109413800e21cd1
Raquel Ururahy <raq.ururahy@gmail.com>
2023-07-20 22:09:54 -0300

    primeiro commit

---
+++ index.py
@@ -0,0 +1,3 @@
+n = int(input('Digite um numero '))
+#print ('O antessesor é ', n-1, 'O sucessor é ', n+1)
+print('Analisando o valor {}, seu antecessor é {} e o sucessor é {}' .format(n, (n-1), (n+1)))
